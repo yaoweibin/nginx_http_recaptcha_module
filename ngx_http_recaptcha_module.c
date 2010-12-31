40022fbaa2c0f449eb20813a8a678c4f0eff7b41
Weibin Yao <yaoweibin@gmail.com>
2010-12-31 07:10:52 +0000

    first compile succ

--- ngx_http_recaptcha_module.c
+++ ngx_http_recaptcha_module.c
@@ -6,37 +6,35 @@
 
 
 typedef struct {
-    ngx_str_t  challenge;
-    ngx_str_t  response;
     ngx_uint_t body_index;
 } ngx_http_recaptcha_conf_t;
 
 
-static ngx_int_t ngx_http_recaptcha_challenge_variable(ngx_http_request_t *r,
+static ngx_int_t ngx_http_recaptcha_form_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data);
-static ngx_int_t ngx_http_recaptcha_response_variable(ngx_http_request_t *r,
-    ngx_http_variable_value_t *v, uintptr_t data);
+
+static char * ngx_http_add_form_variable(ngx_conf_t *cf, ngx_command_t *cmd,
+        void *conf);
 
 static void *ngx_http_recaptcha_create_conf(ngx_conf_t *cf);
 static char *ngx_http_recaptcha_merge_conf(ngx_conf_t *cf, void *parent,
     void *child);
-static ngx_int_t ngx_http_recaptcha_add_variables(ngx_conf_t *cf);
 
 
 static ngx_command_t  ngx_http_recaptcha_commands[] = {
 
     { ngx_string("recaptcha_challenge_name"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
-      ngx_conf_set_str_slot,
+      ngx_http_add_form_variable,
       NGX_HTTP_LOC_CONF_OFFSET,
-      offsetof(ngx_http_recaptcha_conf_t, challenge),
+      0,
       NULL },
 
     { ngx_string("recaptcha_response_name"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
-      ngx_conf_set_str_slot,
+      ngx_http_add_form_variable,
       NGX_HTTP_LOC_CONF_OFFSET,
-      offsetof(ngx_http_recaptcha_conf_t, response),
+      0,
       NULL },
 
       ngx_null_command
@@ -44,7 +42,7 @@ static ngx_command_t  ngx_http_recaptcha_commands[] = {
 
 
 static ngx_http_module_t  ngx_http_recaptcha_module_ctx = {
-    ngx_http_recaptcha_add_variables,        /* preconfiguration */
+    NULL,                                    /* preconfiguration */
     NULL,                                    /* postconfiguration */
 
     NULL,                                    /* create main configuration */
@@ -74,38 +72,95 @@ ngx_module_t  ngx_http_recaptcha_module = {
 };
 
 
-static ngx_str_t  ngx_http_recaptcha_challenge_name = 
-ngx_string("recaptcha_challenge");
-
-static ngx_str_t  ngx_http_recaptcha_response_name =
-ngx_string("recaptcha_set_expires");
-
-
 static ngx_int_t
-ngx_http_recaptcha_challenge_variable(ngx_http_request_t *r,
+ngx_http_recaptcha_form_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data)
 {
-    v->data = NULL;
-    v->len = 1;
+    u_char                     *head, *value, *last;
+    ngx_str_t                  *name;
+    ngx_http_recaptcha_conf_t  *rcf;
+    ngx_http_variable_value_t  *vv;
+
+    rcf = ngx_http_get_module_loc_conf(r, ngx_http_recaptcha_module);
+
+    vv = ngx_http_get_indexed_variable(r, rcf->body_index);
+    if (vv == NULL || vv->not_found || vv->len == 0) {
+        goto not_found;
+    }
+
+    name = (ngx_str_t *) data;
+    if (name == NULL) {
+        goto not_found;
+    }
+
+    ngx_log_debug2(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                   "request_body: \"%*s\", param name = \"%V\"",
+                   vv->len, vv->data, name);
+
+    head = ngx_strnstr((u_char *)vv->data, (char *)name->data, (size_t)vv->len);
+    if (head == NULL) {
+        goto not_found;
+    }
+
+    value = head + name->len;
+
+    if (*value != '=') {
+        goto not_found;
+    }
+
+    value++;
+
+    last = value;
+
+    while (last < (vv->data + vv->len)) {
+
+        if (*last == '&' || *last == CR || *last == LF) {
+            break;
+        }
+
+        last++;
+    }
+
+    v->data = value;
+    v->len = last - value;
     v->valid = 1;
     v->no_cacheable = 0;
     v->not_found = 0;
+
+    return NGX_OK;
+
+not_found:
+
+    v->not_found = 1;
 
     return NGX_OK;
 }
 
 
-static ngx_int_t
-ngx_http_recaptcha_response_variable(ngx_http_request_t *r,
-    ngx_http_variable_value_t *v, uintptr_t data)
+static char * 
+ngx_http_add_form_variable(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-    v->len = 0;
-    v->valid = 1;
-    v->no_cacheable = 0;
-    v->not_found = 0;
-    v->data = NULL;
+    ngx_str_t                 *value, *str;
+    ngx_http_variable_t       *var;
 
-    return NGX_OK;
+    value = cf->args->elts;
+
+    str = ngx_palloc(cf->pool, sizeof(ngx_str_t));
+    if (str == NULL) {
+        return NGX_CONF_ERROR;
+    }
+
+    *str = value[1];
+
+    var = ngx_http_add_variable(cf, str, 0);
+    if (var == NULL) {
+        return NGX_CONF_ERROR;
+    }
+
+    var->get_handler = ngx_http_recaptcha_form_variable;
+    var->data = (uintptr_t) str;
+
+    return NGX_CONF_OK;
 }
 
 
@@ -122,8 +177,6 @@ ngx_http_recaptcha_create_conf(ngx_conf_t *cf)
     /*
      * set by ngx_pcalloc():
      *
-     *     conf->challenge = {0, NULL};
-     *     conf->response = {0, NULL};
      *     conf->body_index = 0;
      */
 
@@ -137,14 +190,7 @@ static char *
 ngx_http_recaptcha_merge_conf(ngx_conf_t *cf, void *parent, void *child)
 {
     ngx_str_t                  body = ngx_string("request_body");
-    ngx_http_recaptcha_conf_t *prev = parent;
     ngx_http_recaptcha_conf_t *conf = child;
-
-    ngx_conf_merge_str_value(conf->challenge, prev->challenge, 
-            "recaptcha_challenge_field");
-
-    ngx_conf_merge_str_value(conf->response, prev->response, 
-            "recaptcha_response_field");
 
     conf->body_index = ngx_http_get_variable_index(cf, &body);
     if (conf->body_index == (ngx_uint_t) NGX_ERROR) {
@@ -154,25 +200,3 @@ ngx_http_recaptcha_merge_conf(ngx_conf_t *cf, void *parent, void *child)
     return NGX_CONF_OK;
 }
 
-
-static ngx_int_t
-ngx_http_recaptcha_add_variables(ngx_conf_t *cf)
-{
-    ngx_http_variable_t  *var;
-
-    var = ngx_http_add_variable(cf, &ngx_http_recaptcha_challenge_name, 0);
-    if (var == NULL) {
-        return NGX_ERROR;
-    }
-
-    var->get_handler = ngx_http_recaptcha_challenge_variable;
-
-    var = ngx_http_add_variable(cf, &ngx_http_recaptcha_response_name, 0);
-    if (var == NULL) {
-        return NGX_ERROR;
-    }
-
-    var->get_handler = ngx_http_recaptcha_response_variable;
-
-    return NGX_OK;
-}
